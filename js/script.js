cd05b8169a12280071eef1030be3fb525cc5bd2f
Daniil Smirnov <suver.www@mail.ru>
2021-03-26 09:00:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let randArray = [];
 
 for (let i = 0; i < 1000; i++){
-    randArray.push(Math.randomInteger(1,100));
+    randArray.push(Math.floor(Math.random() * 100));
 }
 
 function printArray(array=randArray){
